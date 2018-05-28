b60e0ba61c54d2ca9b7a7195b9bcaf436e8173de
mnabdilah <39702438+mnabdilah@users.noreply.github.com>
2018-05-28 22:29:09 +0800

    Add files via upload

---
+++ pekalian_8.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filetugas;
+
+/**
+ *
+ * @author Sabri_abdillah
+ */
+
+public class Tugas2 {
+    public static void main(String[] args) {
+        int [] nilai = new int [10];
+        
+        int j=8;
+        
+        for(int i=1;i<=10;i++){
+            System.out.println(i + "x" + j +"=" +i*j);
+            
+        }
+    }
+    
+}
